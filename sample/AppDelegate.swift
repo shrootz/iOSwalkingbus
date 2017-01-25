1c88cf57473d5720fe696beb06661cd5bc8cd0d0
Subie Madhavan <subie.madhavan@gmail.com>
2017-01-25 15:49:18 -0600

    added a basic google map

--- sample/AppDelegate.swift
+++ sample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyB_PTlTjCvvksraHC4A5jWvuSUchYZrV4o")
+        // GMSPlacesClient.provideAPIKey("AIzaSyB_PTlTjCvvksraHC4A5jWvuSUchYZrV4o")
         // Override point for customization after application launch.
         return true
     }
